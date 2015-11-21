e920c62c2c97d869797189c05d4251e7a0c28d1f
Giovanni Tataranni <g.tataranni@gmail.com>
2015-11-21 20:10:38 +0100

    tried to implement something else for the client, wrote some TODO

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ClientActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ClientActivity.java
@@ -3,8 +3,11 @@ package com.example.mobserv.remoteapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.*;
 import java.net.InetAddress;
@@ -19,15 +22,20 @@ public class ClientActivity extends Activity {
    private Socket socket;
    private static final int serverport = 45678;
    private String serverip;
+   private TextView text;
+   Handler updateConversationHandler;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_client);
-      Intent it = getIntent();
-      this.serverip = it.getStringExtra("serverip");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+        Intent it = getIntent();
+        this.serverip = it.getStringExtra("serverip");
 
-      new Thread(new ClientThread()).start();
+        text = (TextView) findViewById(R.id.idClientText);
+
+        updateConversationHandler = new Handler();
+        new Thread(new ClientThread()).start();
    }
 
    public void onClick(View view) {
@@ -38,6 +46,8 @@ public class ClientActivity extends Activity {
                  new OutputStreamWriter(socket.getOutputStream())),
                  true);
          out.println(str);
+          // TODO find a way to get rid of the \n (print w/o ln doesnt work
+          // TODO reset the edittext hint
       } catch (UnknownHostException e) {
          e.printStackTrace();
       } catch (IOException e) {
@@ -48,23 +58,47 @@ public class ClientActivity extends Activity {
    }
 
    class ClientThread implements Runnable {
+      BufferedReader inputStream;
 
       @Override
       public void run() {
 
          try {
-            InetAddress serverAddr = InetAddress.getByName(serverip);
+             InetAddress serverAddr = InetAddress.getByName(serverip);
+             socket = new Socket(serverAddr, serverport);
+//             this.inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            socket = new Socket(serverAddr, serverport);
-
-         } catch (UnknownHostException e1) {
-            e1.printStackTrace();
-         } catch (IOException e1) {
-            e1.printStackTrace();
+         } catch (Exception e){
+             e.printStackTrace();
+             Toast.makeText(getApplicationContext(), "ERROR :(\n"+e.getMessage(), Toast.LENGTH_SHORT).show();
+             finish();
+             return;
          }
 
+         /*while (!Thread.currentThread().isInterrupted()) {
+            try {
+               String read = inputStream.readLine();
+               updateConversationHandler.post(new updateUIThread(read));
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "ERROR :(\n"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                finish();
+                return;
+            }
+         }*/
       }
-
    }
 
+   class updateUIThread implements Runnable {
+      private String msg;
+
+      public updateUIThread(String str) {
+         this.msg = str;
+      }
+
+      @Override
+      public void run() {
+         text.setText(text.getText().toString()+ msg + "\n");
+      }
+   }
 }
