05ad6916a0a35749008603b70a16c62b2f57240b
Giovanni Tataranni <g.tataranni@gmail.com>
2015-11-22 20:02:42 +0100

    resolved bug related to toasts made from bg thread

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ClientActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ClientActivity.java
@@ -24,8 +24,9 @@ public class ClientActivity extends Activity {
    private String serverip;
    private TextView text;
    Handler updateConversationHandler;
+    EditText et;
 
-   @Override
+    @Override
    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
@@ -33,6 +34,8 @@ public class ClientActivity extends Activity {
         this.serverip = it.getStringExtra("serverip");
 
         text = (TextView) findViewById(R.id.idClientText);
+        et = (EditText) findViewById(R.id.idClientEditText);
+        et.setFocusable(false);
 
         updateConversationHandler = new Handler();
         new Thread(new ClientThread()).start();
@@ -40,20 +43,24 @@ public class ClientActivity extends Activity {
 
    public void onClick(View view) {
       try {
-         EditText et = (EditText) findViewById(R.id.idClientEditText);
-         String str = et.getText().toString();
-         PrintWriter out = new PrintWriter(new BufferedWriter(
-                 new OutputStreamWriter(socket.getOutputStream())),
-                 true);
-         out.println(str);
+        String str = et.getText().toString();
+        PrintWriter out = new PrintWriter(new BufferedWriter(
+          new OutputStreamWriter(socket.getOutputStream())),
+          true);
+
+        out.println(str);
+        //out.write(str); // <-- to be tried
           // TODO find a way to get rid of the \n (print w/o ln doesnt work
-          // TODO reset the edittext hint
+          // TODO reset the edittext hint after sending text
       } catch (UnknownHostException e) {
          e.printStackTrace();
+          runOnUiThread(new makeToast("ERROR:\n" + e.getMessage()));
       } catch (IOException e) {
          e.printStackTrace();
+          runOnUiThread(new makeToast("ERROR:\n" + e.getMessage()));
       } catch (Exception e) {
          e.printStackTrace();
+          runOnUiThread(new makeToast("ERROR:\n" + e.getMessage()));
       }
    }
 
@@ -62,15 +69,23 @@ public class ClientActivity extends Activity {
 
       @Override
       public void run() {
-
+          runOnUiThread(new makeToast("Connecting to " + serverip + ":" + serverport + "..."));
          try {
              InetAddress serverAddr = InetAddress.getByName(serverip);
              socket = new Socket(serverAddr, serverport);
 //             this.inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-         } catch (Exception e){
+             // success =)
+             runOnUiThread(new makeToast("Connected to " + serverAddr + " " + serverport));
+             runOnUiThread(new Runnable() {@Override public void run() {et.setFocusableInTouchMode(true);}});
+         } catch (UnknownHostException e) {
              e.printStackTrace();
-             Toast.makeText(getApplicationContext(), "ERROR :(\n"+e.getMessage(), Toast.LENGTH_SHORT).show();
+             runOnUiThread(new makeToast("ERROR:\n" + e.getMessage()));
+             finish();
+             return;
+         } catch (IOException e) {
+             e.printStackTrace();
+             runOnUiThread(new makeToast("ERROR:\n" + e.getMessage()));
              finish();
              return;
          }
@@ -101,4 +116,15 @@ public class ClientActivity extends Activity {
          text.setText(text.getText().toString()+ msg + "\n");
       }
    }
+
+    class makeToast implements Runnable{
+        private String msg;
+
+        public makeToast(String msg){ this.msg = msg; }
+
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
