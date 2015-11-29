ee993959b3ad7213936fed3f6843e886979ad4c5
pacellig <pacelli92@hotmail.it>
2015-11-29 19:12:08 +0100

    OnBackPressed
    added a dialog when the back button is pressed while in client activity

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ClientActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ClientActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobserv.remoteapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -147,6 +149,20 @@ public class ClientActivity extends Activity {
             Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to close connection to server?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        ClientActivity.super.onBackPressed();
+                    }
+                }).create().show();
+    }
+
 
     /*
     // TODO try if the two overrides wold preserve the connection, but they don't
