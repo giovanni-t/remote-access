0f85d9bfc93c09d3d031cacd960240f0393fdf9f
Giovanni Tataranni <g.tataranni@gmail.com>
2015-12-24 16:56:50 +0100

    Added mock elements in the navigator menu, and also an example of onClickListener

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ChatActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/ChatActivity.java
@@ -1,9 +1,6 @@
 package com.example.mobserv.remoteapp;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 
 public class ChatActivity extends DrawerActivity {
 
--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/DrawerActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/DrawerActivity.java
@@ -5,7 +5,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 /**
@@ -15,7 +20,19 @@ public class DrawerActivity extends AppCompatActivity {
 
     protected DrawerLayout fullLayout;
     protected FrameLayout frameLayout;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private String mActivityTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
 
+    /**
+     * It is fundamental for all the activities that extend this class
+     * to call this setContentView in the onCreate.
+     * It is used to draw the layout of the extending activity and populate
+     * the navigator menu
+     * @param layoutResID
+     */
     @Override
     public void setContentView(int layoutResID) {
         fullLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_drawer, null);
@@ -24,7 +41,47 @@ public class DrawerActivity extends AppCompatActivity {
         super.setContentView(fullLayout);
 
         // Set up the toolbar and the side menu toggle
+
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void addDrawerItems() {
+        final String[] menuArray = { "Ex1", "Ex2", "Ex3", "Ex4", "Ex5" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("DrawerActivity", "Pressed item " + parent.getItemAtPosition(position) + " pos " + position + " with id " + id);
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close){
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.navigator_title);  // we can change the text in the main bar from here
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 }
