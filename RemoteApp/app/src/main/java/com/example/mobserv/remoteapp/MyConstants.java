8775aaf974375f6407615b93d44ec26891ebf2be
Giovanni Tataranni <g.tataranni@gmail.com>
2015-12-27 17:52:48 +0100

    Added constant in a different class, to be used as keys when passing values in Bundles Added colors array for received msg background

---
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MyConstants.java
@@ -0,0 +1,13 @@
+package com.example.mobserv.remoteapp;
+
+/**
+ * Created by giovanni on 27/12/15.
+ */
+public class MyConstants {
+    public static final String TAG_CLIENTS_LIST = "clientsList";
+    public static final String TAG_TEXT_SCROLL_X = "tScrollX";
+    public static final String TAG_TEXT_SCROLL_Y = "tScrollY";
+    public static final String TAG_TASK_FRAGMENT = "task_fragment";
+    public static final String TAG_SERVERIP = "serverip";
+    public static final String TAG_CHAT_FRAGMENT = "chat_fragment";
+}
