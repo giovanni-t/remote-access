15d1e46fb62e8657054df584ea5d503cde70d82a
pacellig <pacelli92@hotmail.it>
2015-12-29 10:19:18 +0100

    first working gallery

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/GalleryActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/GalleryActivity.java
@@ -1,21 +1,39 @@
 package com.example.mobserv.remoteapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class GalleryActivity extends DrawerActivity {
+    private GridView imageGrid;
+    private ArrayList<Bitmap> bitmapList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        this.imageGrid = (GridView) findViewById(R.id.gridview);
+        this.bitmapList = new ArrayList<Bitmap>();
+
         showSavedPictures();
-    }
+
+        this.imageGrid.setAdapter(new ImageAdapter(this, this.bitmapList));
+
+    } // TODO : check array length
 
     public void showSavedPictures(){
         String path = getApplicationContext().getCacheDir() + "/inpaint/";
@@ -23,10 +41,16 @@ public class GalleryActivity extends DrawerActivity {
         File f = new File(path);
         File file[] = f.listFiles();
         Log.d("Files", "Size: "+ file.length);
-        for (int i=0; i < file.length; i++)
-        {
-            Log.d("Files", "FileName:" + file[i].getName());
-            Toast.makeText(getApplicationContext(), file[i].getName(), Toast.LENGTH_SHORT).show();
+
+        try {
+            for(int i = 0; i < file.length; i++) {
+                Log.d("Files", "FileName:" + file[i].getName());
+                Bitmap toadd = BitmapFactory.decodeFile(path+file[i].getName());
+                this.bitmapList.add(toadd);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
