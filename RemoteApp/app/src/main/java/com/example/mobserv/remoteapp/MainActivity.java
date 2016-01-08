62646dc14d6564b200aaf4fe581c93494bf6c1dd
Giovanni Tataranni <g.tataranni@gmail.com>
2016-01-08 20:15:11 +0100

    Removed server and old gui buttons from main activity

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MainActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MainActivity.java
@@ -18,19 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClickServer (View view ){
-        startActivity(new Intent("com.example.mobserv.remoteapp.ServerActivity"));
-    }
-
     public void onClickClient (View view){
-        Intent it = new Intent("com.example.mobserv.remoteapp.ClientActivity");
-        EditText etIpaddr = (EditText) findViewById(R.id.idIpAddrEditText);
-        String address = etIpaddr.getText().toString();
-        it.putExtra(MyConstants.TAG_SERVERIP, address);
-        startActivity(it);
-    }
-
-    public void onClickChatUI (View view){
         Intent it = new Intent(MainActivity.this, ChatActivity.class);
         String address = ((EditText) findViewById(R.id.idIpAddrEditText)).getText().toString();
         it.putExtra(MyConstants.TAG_SERVERIP, address);
