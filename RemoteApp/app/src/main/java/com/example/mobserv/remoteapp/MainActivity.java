6a4221a7820fd1ecc8d9610a3771066ba631824e
pacellig <pacelli92@hotmail.it>
2016-01-15 19:55:25 +0100

    save the last ip connected

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MainActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MainActivity.java
@@ -2,20 +2,30 @@ package com.example.mobserv.remoteapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Main activity of the app.
  * Let the user insert the IP of the running server
  */
 public class MainActivity extends AppCompatActivity {
-
+    String oldAddres = "";
+    TextView twIP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        twIP = (TextView) findViewById(R.id.idIpAddrEditText);
+        oldAddres = PreferenceManager.getDefaultSharedPreferences(this).getString("IP_ADDRESS", "");
+        if (oldAddres.compareTo("")!=0){
+            twIP.setText(oldAddres);
+        } else {
+            twIP.setText("");
+        }
     }
 
     /*
@@ -35,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClickChatUI (View view){
         Intent it = new Intent(MainActivity.this, ChatActivity.class);
         String address = ((EditText) findViewById(R.id.idIpAddrEditText)).getText().toString();
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putString("IP_ADDRESS", address).commit();
         it.putExtra(MyConstants.TAG_SERVERIP, address);
         startActivity(it);
     }
