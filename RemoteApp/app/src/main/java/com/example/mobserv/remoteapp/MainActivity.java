f9eea7b5d2a121e27ecda28b3bb36c0b9eb08d3e
pacellig <pacelli92@hotmail.it>
2016-02-08 18:13:06 +0100

    icon fix

--- RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MainActivity.java
+++ RemoteApp/app/src/main/java/com/example/mobserv/remoteapp/MainActivity.java
@@ -54,8 +54,6 @@ public class MainActivity extends AppCompatActivity {
         it.putExtra(MyConstants.TAG_SERVERIP, address);
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        //NetworkInfo mMobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
         if (mWifi.isConnected() == false) {
             Toast.makeText(this, R.string.not_wifi_connected, Toast.LENGTH_LONG).show();
         } else {
